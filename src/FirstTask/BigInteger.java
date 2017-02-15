a1ebdfcb70f11b7320069078c1bd4c68c4b1dc34
Grafov Denis <grafov.denis@gmail.com>
2017-02-16 01:19:35 +0300

    constructors done (probably)

--- src/FirstTask/BigInteger.java
+++ src/FirstTask/BigInteger.java
@@ -1,4 +1,42 @@
 package FirstTask;
 
-public class BigInteger {
+import java.util.ArrayList;
+
+public final class BigInteger {
+
+    private int[] array;
+
+    //конструктор (строковый)
+    public BigInteger(String str) {
+        this.array = str.chars().map(c -> c -= '0').toArray();
+    }
+    //конструктор (из числа)
+    public BigInteger(int num) {
+        this.array = Integer.toString(num).chars().map(c -> c -= '0').toArray();
+    }
+
+    //equals
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj instanceof BigInteger) {
+            BigInteger other = (BigInteger) obj;
+            return array == other.array;
+        }
+        return false;
+    }
+    //less
+
+    //more
+
+    //plus
+
+    //minus
+
+    //multiply
+
+    //divide
+
+    //mod
+
 }
