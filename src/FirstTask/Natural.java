6fe2759677187a9860f756d6b5941dffb94f2155
Grafov <grafov.denis@gmail.com>
2017-03-09 11:33:51 +0300

    Natural.minus added

--- src/FirstTask/Natural.java
+++ src/FirstTask/Natural.java
@@ -41,7 +41,7 @@ public final class Natural {
 
     //less
     public boolean less(Natural other) {
-        if (array.length > other.array.length) {
+        if (array.length >= other.array.length) {
             return false;
         } else if (array.length < other.array.length) {
             return true;
@@ -106,8 +106,42 @@ public final class Natural {
         return new Natural(result);
     }
 
-    //minus
+    //deleteZeros
 
+    //minus
+    public Natural minus(Natural other) {
+        ArrayList<Integer> a = new ArrayList<>();
+        ArrayList<Integer> b = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int el : array) {
+            a.add(el);
+        }
+        for (int i = 0; i < array.length - other.array.length; i++) {
+            b.add(0);
+        }
+        for (int el : other.array) {
+            b.add(el);
+        }
+        for (int i = 0; i < a.size(); i++) {
+            int temp = a.get(a.size() - 1 - i) - b.get(a.size() - i - 1);
+            result.add(i, temp);
+        }
+        for (int i = 0; i < result.size() - 1; i++) {
+            if (result.get(i) < 0) {
+                int temp = result.get(i) + 10;
+                result.remove(i);
+                result.add(i, temp);
+                temp = result.get(i + 1) - 1;
+                result.remove(i + 1);
+                result.add(i + 1, temp);
+            }
+        }
+        Collections.reverse(result);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(result);
+        return new Natural(result);
+    }
     //multiply
 
     //divide
@@ -129,11 +163,11 @@ public final class Natural {
     }
 
     public static void main(String[] args) {
-        Natural b1 = new Natural("345671");
-        Natural b2 = new Natural("354671");
+        Natural b1 = new Natural("132");
+        Natural b2 = new Natural("132");
         System.out.println(b1);
         System.out.println(b2);
-        System.out.println(b1.plus(b2));
+        System.out.println(b1.minus(b2));
         System.out.println(b1.more(b2));
         System.out.println(b1.less(b2));
     }
